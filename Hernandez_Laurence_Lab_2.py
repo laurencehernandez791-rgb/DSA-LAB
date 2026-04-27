6cb99db540ab51d664644caa9da2ed6955de7467
laurencehernandez791-rgb <https://github.com/laurencehernandez791-rgb>
2026-04-27 13:00:11 +0800

    add inditation to insert after given node

--- Hernandez_Laurence_Lab_2.py
+++ Hernandez_Laurence_Lab_2.py
@@ -1,64 +1,68 @@
 class Node:
-    def __init__(self,data):
-        self.data = data
-        self.text = None 
-nodel = Node('Kabisado by IV OF SPADES')
-head = nodel 
+    def __init__(self, data):
+        self.data = data 
+        self.next = None
+        
+        
 def insertNodeAtTheBeginning(data):
     global head
     newNode = Node(data)
 
-    if(head == None): 
+    if(head == None):
+        head = newNode
+    else:
+        newNode.next = head
         head = newNode 
-    else: 
-      newNode.next = head 
-      head = newNode 
+
+
 def insertNodeAtTheEnd(data):
     global head 
-newNode = Node(data)
-if (head == None): 
-   head= nesNode 
-else:
-   current = head 
-   while(current.next):
-      current = current.next 
-      current.next = newNode 
-def traverseLinkedList():
-    current = head 
-    while(current): 
-         print(current.data, end= " -> ")
-         current = current.next 
-         
+    newNode = Node(data)
 
+    if(head == None):
+        head= newNode
+    else:
+        current = head
+        while(current.next):
+            current = current.next
+        current.next = newNode
+
+def traverseLinked():
+    current = head
+    while(current):
+        print(current.data, end=" ->")
+        current = current.next
+        
+        
 def insertAfterGivennode(data, node):
-     global head 
-     newNode = Node(data)
+    global head
+    newNode = Node(data)
 
-if(head == None): 
-      head = newNode 
-else: 
-     current = head 
-     prev = head 
+    if(head == None):
+        head = newNode
+    else:
+        current = head
+        prev = head
 
-while(prev.data != node): 
-      prev = current 
-      current = current.next 
+    while(prev.data != node):
+        prev = current
+        current = current.next
 
-if(current == None):
- print('The node does not exist')
- 
-      
-newNode.next = current 
-prev.next = newNode
-
-nodel = Node('Risk It All by brunomars')
-head = nodel 
+        if(current == None):
+            print('The node does not exist')
+            return
+        
+    newNode.next = current 
+    prev.next = newNode
+  
+node1 = Node('Risk It All by brunomars')
+head = node1
 insertNodeAtTheBeginning('Makaluma by Wilbert Ross')
 insertNodeAtTheBeginning('To The Bone by PAMUNGKAS')
-insertNodeAtTheBeginning('Palayo sa Mundo by ARTHUR')
+insertNodeAtTheBeginning('Pagsamo by ARTHUR')
 
 insertNodeAtTheEnd('Palayo sa Mundo by Moira DELA TORRE')
-insertNodeAtTheEnd( 'Paalam,Leonora by Sugarcane')
+insertNodeAtTheEnd('Paalam,Leonora by Sugarcane')
 
-insertAfterGivennode ('count on me by bruno mars, just the way you are by brunomars')
-traverseLinked() 
+insertAfterGivennode('count on me by bruno mars', 'just the way you are by brunomars')
+traverseLinked()
